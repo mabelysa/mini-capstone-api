bc2d8f2696c6b0d59a8a41dcf9118c2a40987ece
Mabelys Arias <MabelysA@gmail.com>
2021-06-16 14:44:58 -0400

    creates routes that displays json for individual products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.create(name: "body_butter", price: 25. image_url: "", description)
+product = Product.create(name: "body_butter", price: 25, image_url: "", description: "silky smooth coating for all your dry days.")
+
+product = Product.create(name: "body_spritz", price: 20, image_url: "", description: "to sparkle and shine like the star that you are!")
+
+product = Product.create(name: "body_lotion", price: 22, image_url: "", description: "a light coating to make your skin feel supple and new.")
