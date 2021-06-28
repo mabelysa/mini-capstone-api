9d1dec187f65618e3746dfcb3b85ebab59b64223
Mabelys Arias <MabelysA@gmail.com>
2021-06-28 12:39:44 -0400

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,10 @@ Rails.application.routes.draw do
   get "/suppliers" => "suppliers#index"
 
   get "/suppliers/:id" => "suppliers#show"
+
+  post "/suppliers" => "suppliers#create"
+
+  patch "/suppliers/:id" => "suppliers#update"
+
+  delete "/suppliers/:id" => "suppliers#destroy"
 end
